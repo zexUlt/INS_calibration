6162b39343dc830523160f5e9babc2fa1f45abb3
ZexUlt <danil.chernozubov66@gmail.com>
2022-04-19 23:23:35 +0300

    Initial commit

---
+++ ins.py
@@ -0,0 +1,172 @@
+from os import sep
+import pandas as pd
+# from numрy import *
+import numpy as np
+import matplotlib.pyplot as plt
+
+__S_f1__ = None
+__b_f1__ = None
+
+def accel_calibration_1():
+    # z+, z-, x+, x-, y+, y-
+    df = pd.read_csv('ins_data/second_accel.csv', sep=';', dtype=np.int32)
+
+    a1 = df.ACC1
+    a2 = df.ACC2
+    a3 = df.ACC3
+
+    a_slices = [slice(1300, 1600), slice(2200, 2500), slice(0, 300), slice(1800, 2100), slice(2700, 3000), slice(700, 1000)]
+
+    raw_data = np.array([[x,y,z] for x,y,z in zip(a1, a2, a3)])
+
+    averaged_a1 = np.array(list(map(lambda x: a1[x].mean(), a_slices)))
+    averaged_a2 = np.array(list(map(lambda x: a2[x].mean(), a_slices)))
+    averaged_a3 = np.array(list(map(lambda x: a3[x].mean(), a_slices)))
+
+    g = 9.81
+
+    a_minus = np.array(
+        [
+            averaged_a1[3:],
+            averaged_a2[3:],
+            averaged_a3[3:],
+        ]
+    )
+
+    a_plus = np.array(
+        [
+            averaged_a1[:3],
+            averaged_a2[:3],
+            averaged_a3[:3],
+        ]
+    )
+
+    print(a_plus, a_minus, sep='\n')
+
+    S_a = (a_minus - a_plus) / 2 / g
+
+
+    b_a = (a_plus + a_minus) / 2
+
+    S_f = np.linalg.inv(S_a)
+    b_f = -S_f @ b_a
+
+    __S_f1__ = S_f
+    __b_f1__ = b_f
+    
+    aggregated_data = np.array([S_f @ x + b_f[:, 0] for x in raw_data])
+    
+    plt.subplot(3, 2, 1)
+    plt.plot(df.ACC1)
+    plt.legend('acc1 raw')
+    plt.subplot(3, 2, 3)
+    plt.plot(df.ACC2)
+    plt.legend('acc2 raw')
+    plt.subplot(3, 2, 5)
+    plt.plot(df.ACC3)
+    plt.legend('acc3 raw')
+    plt.xlabel('Clocks')
+    
+    
+    plt.subplot(3, 2, 2)
+    plt.plot(aggregated_data[:, 0], color='g')
+    plt.subplot(3, 2, 4)
+    plt.plot(aggregated_data[:, 1], color='g')
+    plt.subplot(3, 2, 6)
+    plt.plot(aggregated_data[:, 2], color='g')
+    plt.xlabel('Clocks')
+    plt.show()
+
+
+def gyro_calibration():
+    # z+ z- y+ y- x+ x-
+    df = pd.read_csv('./ins_data/first_gyro.csv', sep=';', dtype=np.int32)
+
+    g1 = df.GYRO1
+    g2 = df.GYRO2
+    g3 = df.GYRO3
+  
+    w_slices = [slice(3900, 4100), slice(4630, 4770), slice(2540, 2680), slice(1890, 2010), slice(700, 830), slice(4390, 4480)] 
+
+    b_w = np.array([g1[400:1000].mean(), g2[400:1000].mean(), g3[2800:3000].mean()])
+
+    g1 -= b_w[0]
+    g2 -= b_w[1]
+    g3 -= b_w[2]
+
+    raw_data = np.array([[x,y,z] for x,y,z in zip(g1, g2, g3)])
+
+    averaged_g1 = np.array(list(map(lambda x: g1[x].mean(), w_slices)))
+    averaged_g2 = np.array(list(map(lambda x: g2[x].mean(), w_slices)))
+    averaged_g3 = np.array(list(map(lambda x: g3[x].mean(), w_slices)))
+
+    phi_minus = np.array(
+        [
+            averaged_g1[::2],
+            averaged_g2[::2],
+            averaged_g3[::2]
+        ]
+    )
+
+    phi_plus = np.array(
+        [
+            averaged_g1[1::2],
+            averaged_g2[1::2],
+            averaged_g3[1::2]
+        ]
+    )
+    
+    Sw = (phi_minus - phi_plus) / np.deg2rad(180)
+    S_w = np.linalg.inv(Sw)
+
+    
+    aggregated_data = np.array([S_w @ x for x in raw_data])
+
+    print(S_w)
+
+    plt.subplot(3, 2, 1)
+    plt.plot(g1)
+    plt.subplot(3, 2, 3)
+    plt.plot(g2)
+    plt.subplot(3, 2, 5)
+    plt.plot(g3)
+    
+    plt.subplot(3, 2, 2)
+    plt.plot(aggregated_data[:, 0], color='r')
+    plt.subplot(3, 2, 4)
+    plt.plot(aggregated_data[:, 1], color='g')
+    plt.subplot(3, 2, 6)
+    plt.plot(aggregated_data[:, 2], color='y')
+    plt.show()
+
+
+def accel_calibration_2():
+    df = pd.read_csv('ins_data/third_accel_18.csv', sep=';', dtype=np.int32)
+
+    a1 = df.ACC1
+    a2 = df.ACC2
+    a3 = df.ACC3
+
+    a_slices = [slice(1, 300), slice(700, 980), slice(1090, 1465), slice(1580, 1980), slice(2315, 3040), slice(3170, 3510) ]
+    a1 = [mean(X);mean(Y(1:300));mean(Z(1:300))];
+a2 = [mean(X);mean(Y(700:980));mean(Z(700:980))];
+a3 = [mean(X);mean(Y(1090:1465));mean(Z(1090:1465))];
+a4 = [mean(X);mean(Y(1580:1980));mean(Z(1580:1980))];
+a5 =  [mean(X);mean(Y(2315:3040));mean(Z(2315:3040))];
+a6 = [mean(X);mean(Y(3170:3510));mean(Z(3170:3510))];
+a7 = [mean(X(3610:4000));mean(Y(3610:4000));mean(Z(3610:4000))];
+a8 = [mean(X(4100:4640));mean(Y(4100:4640));mean(Z(4100:4640))];
+a9 =  [mean(X(4715:5145));mean(Y(4715:5145));mean(Z(4715:5145))];
+a10 = [mean(X(5230:5680));mean(Y(5230:5680));mean(Z(5230:5680))];
+a11 = [mean(X(5775:6170));mean(Y(5775:6170));mean(Z(5775:6170))];
+a12 = [mean(X(6260:7530));mean(Y(6260:7530));mean(Z(6260:7530))];
+a13 = [mean(X(7700:8330));mean(Y(7700:8330));mean(Z(7700:8330))];
+a14 = [mean(X(8450:8860));mean(Y(8450:8860));mean(Z(8450:8860))];
+a15 = [mean(X(8945:9440));mean(Y(8945:9440));mean(Z(8945:9440))];
+a16 = [mean(X(9530:9940));mean(Y(9530:9940));mean(Z(9530:9940))];
+a17 = [mean(X(10040:10480));mean(Y(10040:10480));mean(Z(10040:10480))];
+a18 = [mean(X(10580:10970));mean(Y(10580:10970));mean(Z(10580:10970))];
+
+if __name__ == "__main__":
+    accel_calibration_1()
+    gyro_calibration()
